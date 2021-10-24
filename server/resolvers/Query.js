5f1159d92989e974d0964667243d3315773bde5c
Luca Di Fazio <highliuk@gmail.com>
2021-10-24 15:39:11 +0200

    Author, Authors root queries

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -9,7 +9,17 @@ function book(parent, { id }) {
   return _.find(db.books, { id })
 }
 
+function authors() {
+  return db.authors
+}
+
+function author(parent, { id }) {
+  return _.find(db.authors, { id })
+}
+
 module.exports = {
   books,
   book,
+  authors,
+  author,
 }
--- server/schema.js
+++ server/schema.js
@@ -4,12 +4,20 @@ const typeDefs = gql`
   type Query {
     books: [Book!]!
     book(id: ID!): Book
+    authors: [Author!]!
+    author(id: ID!): Author
   }
 
   type Book {
     id: ID!
     name: String!
     genre: String!
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    age: Int!
   }
 `
 

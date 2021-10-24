5a85ef232aed9d9f1cf462136b0fc0705dd3b6a3
Luca Di Fazio <highliuk@gmail.com>
2021-10-24 11:50:57 +0200

    Book root query

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -4,6 +4,11 @@ function books() {
   return db.books
 }
 
+function book(_, { id }) {
+  return db.books.filter((book) => book.id === id)?.[0]
+}
+
 module.exports = {
   books,
+  book,
 }
--- server/schema.js
+++ server/schema.js
@@ -3,6 +3,7 @@ const { gql } = require("apollo-server")
 const typeDefs = gql`
   type Query {
     books: [Book!]!
+    book(id: ID!): Book
   }
 
   type Book {
